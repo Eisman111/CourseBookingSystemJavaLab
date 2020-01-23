6cb8da556bb6b619921a5b0bea3aa67c87eeaf26
L3n1ad <danieltoth0910@gmail.com>
2020-01-23 11:04:38 +0000

    create booking class and repository

---
+++ src/main/java/com/codeclan/example/coursebookingsystem/models/Booking.java
@@ -0,0 +1,36 @@
+package com.codeclan.example.coursebookingsystem.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String date;
+
+    public Booking(){}
+
+    public Booking(String date) {
+        this.date = date;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
--- src/test/java/com/codeclan/example/coursebookingsystem/CoursebookingsystemApplicationTests.java
+++ src/test/java/com/codeclan/example/coursebookingsystem/CoursebookingsystemApplicationTests.java
@@ -1,6 +1,8 @@
 package com.codeclan.example.coursebookingsystem;
 
+import com.codeclan.example.coursebookingsystem.models.Booking;
 import com.codeclan.example.coursebookingsystem.models.Course;
+import com.codeclan.example.coursebookingsystem.repositories.BookingRepository;
 import com.codeclan.example.coursebookingsystem.repositories.CourseRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,18 +13,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class CoursebookingsystemApplicationTests {
 
+	private CourseRepository courseRepository;
+	private BookingRepository bookingRepository;
+
 	@Autowired
-	CourseRepository courseRepository;
+	public CoursebookingsystemApplicationTests(CourseRepository courseRepository, BookingRepository bookingRepository) {
+		this.courseRepository = courseRepository;
+		this.bookingRepository = bookingRepository;
+	}
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	void canCreateCourse(){
+	void canCreateCourseAndBooking(){
 		Course course = new Course("course1", "Edinburgh", 2);
 		courseRepository.save(course);
 		assertEquals(1, courseRepository.findAll().size());
+
+		Booking booking = new Booking("12-23-2018");
+		bookingRepository.save(booking);
+		assertEquals(1, bookingRepository.findAll().size());
 	}
 
 }
